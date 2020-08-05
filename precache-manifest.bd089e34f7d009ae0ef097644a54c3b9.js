205504ebd104aae357c3306ea8bb31f18362f28c
cezary-gawronski <idetepowiec@gmail.com>
2020-08-05 16:59:09 +0200

    Updates

---
+++ precache-manifest.bd089e34f7d009ae0ef097644a54c3b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd5a360405b58fb2312a099320006ac",
+    "url": "/todos-list-react/index.html"
+  },
+  {
+    "revision": "20ce21b68ae884ca77bb",
+    "url": "/todos-list-react/static/css/main.d2afc996.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ce21b68ae884ca77bb",
+    "url": "/todos-list-react/static/js/main.0ddc7fdd.chunk.js"
+  },
+  {
+    "revision": "97d8fa25e5bde6a122ac",
+    "url": "/todos-list-react/static/js/runtime-main.bb6efc59.js"
+  },
+  {
+    "revision": "d98b42d1368e4f077405b77b381e944c",
+    "url": "/todos-list-react/static/media/no-entry_26d4.d98b42d1.png"
+  }
+]);
